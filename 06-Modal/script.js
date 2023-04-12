b8d455891c2d79e18fee865e6149d271337a04dc
alex <907785012@qq.com>
2023-04-12 16:48:04 +0800

    complete the modal project

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -1,11 +1,32 @@
 "use strict";
 
-const modal = document.querySelector("modal");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const closeModal = document.querySelector(".close-modal");
 const showModal = document.querySelectorAll(".show-modal");
-const closeModal = document.querySelectorAll(".close-modal");
-const overlay = document.querySelectorAll(".overlay");
+
+// Close modals
+const closeModalFunc = () => {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+
+// Open modals
+const openModalFunc = () => {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
 
 // Traverse show modals
 for (let curModal of showModal) {
-    console.log(curModal.textContent);
+    curModal.addEventListener("click", openModalFunc);
 }
+
+closeModal.addEventListener("click", closeModalFunc);
+overlay.addEventListener("click", closeModalFunc);
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+        closeModalFunc();
+    }
+});
