915352d90179607f2fe16f7436a3b18c351b634e
alex <907785012@qq.com>
2023-07-15 17:42:39 +0800

    Complete forkify search implementation

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -1,13 +1,57 @@
-const recipeContainer = document.querySelector('.recipe');
+import { async } from "regenerator-runtime";
+import * as model from "./model.js";
+import recipeView from "./views/recipeView.js";
+import searchView from "./views/searchView.js";
+import resultsView from "./views/resultsView.js";
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+if (module.hot) {
+    module.hot.accept();
+}
+
+const controlRecipes = async function () {
+    try {
+        const id = window.location.hash.slice(1);
+
+        if (!id) {
+            return;
+        }
+        recipeView.renderSpinner();
+
+        // 1. Loading recipe
+        await model.loadRecipe(id);
+
+        // 2. Rendering recipe
+        recipeView.render(model.state.recipe);
+    } catch (error) {
+        recipeView.renderError();
+    }
 };
 
-// https://forkify-api.herokuapp.com/v2
+const controlSearchResults = async function () {
+    try {
+        resultsView.renderSpinner();
 
-///////////////////////////////////////
+        // Get search query
+        const query = searchView.getQuery();
+        if (!query) {
+            return;
+        }
+
+        // Load the search results
+        await model.loadSearchResults(query);
+
+        // Render results
+        resultsView.render(model.state.search.results);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const init = function () {
+    recipeView.addHandlerRender(controlRecipes);
+    searchView.addHandlerSearch(controlSearchResults);
+};
+init();
---
+++ 18-forkify/src/js/model.js
@@ -0,0 +1,49 @@
+import { API_URL } from "./config.js";
+import { getJSON } from "./helper.js";
+
+export const state = {
+    recipe: {},
+    search: {
+        query: "",
+        results: [],
+    },
+};
+
+export const loadRecipe = async function (id) {
+    try {
+        const data = await getJSON(`${API_URL}${id}`);
+
+        const { recipe } = data.data;
+        state.recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients,
+        };
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const loadSearchResults = async function (query) {
+    try {
+        state.search.query = query;
+
+        const data = await getJSON(`${API_URL}?search=${query}`);
+
+        state.search.results = data.data.recipes.map((recipe) => {
+            return {
+                id: recipe.id,
+                title: recipe.title,
+                publisher: recipe.publisher,
+                image: recipe.image_url,
+            };
+        });
+    } catch (error) {
+        throw error;
+    }
+};
