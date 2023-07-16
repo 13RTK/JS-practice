6df98c91fac54fff1acb61558611974cd54d8c91
alex <907785012@qq.com>
2023-07-16 22:45:06 +0800

    Complete serving update part

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -1,15 +1,11 @@
-import { async } from "regenerator-runtime";
 import * as model from "./model.js";
 import recipeView from "./views/recipeView.js";
 import searchView from "./views/searchView.js";
 import resultsView from "./views/resultsView.js";
+import paginationView from "./views/paginationView.js";
 
 import "core-js/stable";
 import "regenerator-runtime/runtime";
-
-if (module.hot) {
-    module.hot.accept();
-}
 
 const controlRecipes = async function () {
     try {
@@ -44,14 +40,33 @@ const controlSearchResults = async function () {
         await model.loadSearchResults(query);
 
         // Render results
-        resultsView.render(model.state.search.results);
+        resultsView.render(model.getSearchResultsPage());
+
+        // Render pagination
+        paginationView.render(model.state.search);
     } catch (error) {
         console.error(error);
     }
 };
 
+const controlPagination = function (goToPage) {
+    resultsView.render(model.getSearchResultsPage(goToPage));
+    paginationView.render(model.state.search);
+};
+
+const controlServings = function (newServings) {
+    // Update servings in state
+    model.updateServings(newServings);
+
+    // Update the recipe view
+    recipeView.render(model.state.recipe);
+};
+
 const init = function () {
     recipeView.addHandlerRender(controlRecipes);
+    recipeView.addHandlerUpdateServings(controlServings);
     searchView.addHandlerSearch(controlSearchResults);
+    paginationView.addHandlerClick(controlPagination);
 };
+
 init();
